2a87a2e337b126cab8e7f475cbade956bdcee4c8
Noelle <ndaehler@bsd220.org>
2020-01-28 10:42:46 -0600

    Initial Commit

---
+++ Top Stories/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Top Stories
+//
+//  Created by Noelle Daehler on 1/28/20.
+//  Copyright © 2020 Noelle Daehler. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
