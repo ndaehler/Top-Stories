4ec71e1e3a1b1f5b2e661ff60f2e3de213d63713
Noelle <ndaehler@bsd220.org>
2020-01-29 10:50:29 -0600

    finished project

--- Top Stories/SourcesViewController.swift
+++ Top Stories/SourcesViewController.swift
@@ -9,22 +9,22 @@
 import UIKit
 
 class SourcesViewController: UITableViewController {
-
+    
     var sources = [[String : String]]()
     let apiKey = "5d892509a49046a087917c466fa80d09"
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.title = "News Sources"
-          let query = "https://newsapi.org/v1/sources?language=en&country=us&apiKey=\(apiKey)"
+        let query = "https://newsapi.org/v1/sources?language=en&country=us&apiKey=\(apiKey)"
         if let url = URL(string: query) {
-           if let data = try? Data(contentsOf: url) {
-              let json = try! JSON(data: data)
-              if json["status"] == "ok" {
-                 parse(json: json)
-                 return
-              }
-           }
+            if let data = try? Data(contentsOf: url) {
+                let json = try! JSON(data: data)
+                if json["status"] == "ok" {
+                    parse(json: json)
+                    return
+                }
+            }
         }
         loadError()
     }
@@ -41,11 +41,21 @@ class SourcesViewController: UITableViewController {
     }
     
     func loadError() {
-       let alert = UIAlertController(title: "Loading Error",
-                                   message: "There was a problem loading the news feed",
-                            preferredStyle: .actionSheet)
-       alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
-       present(alert, animated: true, completion: nil) }
+        let alert = UIAlertController(title: "Loading Error",
+                                      message: "There was a problem loading the news feed",
+                                      preferredStyle: .actionSheet)
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+        present(alert, animated: true, completion: nil) }
     
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return sources.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        let source = sources[indexPath.row]
+        cell.textLabel?.text = source["name"]
+        cell.detailTextLabel?.text = source["description"]
+        return cell
+    }
 }
-
